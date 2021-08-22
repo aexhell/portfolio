2cf39330ee5a66c6b826b6e849131016eab08d6b
aex <hi@aexhell.ml>
2021-08-22 05:15:04 +0200

    added my own svg

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -19,7 +19,25 @@
             <div class="topbar-github"><a href="https://github.com/aexhell"><GithubIcon size="1.5x" /> <span>GitHub</span></a></div>
          </div>
          <div class="topbar-icon" v-on:click="changeLang">
-            <div class="topbar-lang"><ItalicIcon size="1.5x" /> <span> {{ $t('page.topbar.lang') }}</span></div>
+            <div class="topbar-lang">
+               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 22" fill="none">
+                  <g clip-path="url(#clip0)">
+                     <path d="M1.41801 15.0156C1.41801 15.0156 8.33833 8.71569 9.77722 4.17959L1.418 4.17959" stroke="currentColor" stroke-linecap="round"/>
+                     <path d="M3.58521 6.6564C3.58521 6.6564 4.19645 8.85059 5.75242 10.3716C7.30839 11.8926 10.3964 12.2292 10.3964 12.2292" stroke="currentColor" stroke-linecap="round"/>
+                     <path d="M1 4.17999L11 4.17999" stroke="currentColor" stroke-linecap="round"/>
+                     <line x1="6" y1="3.51508" x2="6" y2="2.50001" stroke="currentColor" stroke-linecap="round"/>
+                     <path d="M11.7483 16.1042L14.2483 16.1042L16.7483 16.1042" stroke="currentColor" stroke-linecap="round"/>
+                     <path d="M10 18.9421L14.7483 9.10419" stroke="currentColor" stroke-linecap="round"/>
+                     <path d="M18.2483 18.6042L14.7483 9.10424" stroke="currentColor" stroke-linecap="round"/>
+                  </g>
+                  <defs>
+                     <clipPath id="clip0">
+                        <rect width="24" height="24" fill="white"/>
+                     </clipPath>
+                  </defs>
+               </svg>
+               <span> {{ $t('page.topbar.lang') }}</span>
+            </div>
          </div>
       </div>
    </div>
