859b01562775ec88ce69458b0fb073668fac9015
aex <hi@aexhell.ml>
2021-10-22 11:50:47 +0200

    layouts(error) ── 404 page done

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="phb__mount" class="p-24 flex-col w-screen h-screen flex justify-center items-center">
-    <h1 id="phb__header" class="text-2xl text-red-400">
-      ~$ 404
-    </h1>
-    <div id="phb__content">
+    <div id="phb__content" class="flex flex-col justify-center items-center">
+      <h1 id="phb__header" class="text-2xl text-red-400 font-500">
+        ~$ 404
+      </h1>
       <p>page not found.</p>
-      <div id="phb__button" class="mt-8 flex justify-center items-center cursor-pointer bg-button hover:bg-indigo-700 p-4 rounded-lg">
+      <div id="phb__button" class="mt-8 flex justify-center items-center cursor-pointer bg-button hover:bg-btn_hover p-4 rounded-lg">
         <nuxt-link to="/" class="text-white hover:no-underline">
           ../home
         </nuxt-link>
@@ -14,13 +14,14 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
-
-export default defineComponent({
-  meta: {
-    title: 'Not Found',
-    namespace: 'normal'
+<script>
+export default {
+  name: 'Error',
+  transition (to, from) {
+    if (!from) {
+      return 'slide-left'
+    }
+    return +to.query.page < +from.query.page ? 'slide-right' : 'slide-left'
   }
-})
+}
 </script>
