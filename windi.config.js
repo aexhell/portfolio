f1a933c38bd1c28d72f500548cec36cafc13a359
aex <hi@aexhell.ml>
2021-11-06 00:53:15 +0100

    chore ── fix netlify start error

--- windi.config.js
+++ windi.config.js
@@ -13,12 +13,12 @@ export default {
       button: '#2D2934',
       btn_hover: '#37333E',
       main: '#A39DD3',
-      gray: colors.gray,
       grey: {
         1: '#201F28',
         2: '#272635',
         3: '#6F73A0'
-      }
+      },
+      ...colors
     }
   }
 }
