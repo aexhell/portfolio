53e30159adcb56e56b9b5f9b05cfd52c90508147
ㅤㅤ <hi@aexhell.ml>
2022-02-26 00:58:55 +0100

    chore: moving custom font to link data

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,9 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap' }
     ]
   },
   globalName: 'phobos',
