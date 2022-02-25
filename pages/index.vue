f3bfe026d4b7ee65ff627130a6b09ff1d96a4f55
ㅤㅤ <hi@aexhell.ml>
2022-02-25 13:54:49 +0100

    pages: for a better day, for a broken sky.

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="phb__home" class="flex w-screen h-screen flex-row justify-center items-center">
-    <div class="phb__post-container flex flex-row">
-      <img class="w-16 h-16 rounded-full mr-4" src="avatar.png">
-      <div class="phb__user-details flex flex-col">
-        <h1 class="text-white font-medium text-xl">
-          aexhell
-        </h1>
-        <span class="text-midnight-400">@aurora</span>
-        <p class="mt-4">
-          coming soon
-        </p>
+    <div class="phb__post-container flex flex-col justify-center items-center">
+      <div class="phb__post-details">
+        <span class="text-midnight-400 text-sm">>> [#] 2/25/2022 1:35 PM</span>
+      </div>
+      <p class="text-midnight-900">
+        [耒] / for a better day. for a broken sky.
+      </p>
+      <div class="phb__social mt-8 text-midnight-500 text-sm">
+        <a href="https://github.com/aexhell">
+          因果
+        </a>
       </div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,16 @@ module.exports = {
           100: '#272130',
           200: '#2d2638',
           300: '#433B50',
-          400: '#635676'
+          400: '#635676',
+          500: '#8875A5',
+          600: '#B9A0DF',
+          700: '#C2AFDF',
+          800: '#CCBEE1',
+          900: '#E5D3FF'
         }
       },
       fontFamily: {
-        sans: ['Inter', '"Public Sans"', 'sans-serif']
+        sans: ['IBM Plex Mono', 'Fira Code', 'sans-serif']
       }
     }
   },
