cda022c4c6eef896363884d3b09c287817439357
ㅤㅤ <hi@aexhell.ml>
2022-02-26 00:52:59 +0100

    chore: add more phrases

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <span class="text-midnight-400 text-sm">>> [#] 2/25/2022 1:35 PM</span>
       </div>
       <p v-if="showMsg" class="text-midnight-900">
-        [耒] / {{ messages[Math.floor(Math.random() * messages.length)] }}
+        [耒] / {{ messages[phrase ? phrase : Math.floor(Math.random() * messages.length)] }}
       </p>
       <p v-else class="text-midnight-900">
         [耒]
@@ -24,6 +24,7 @@ export default {
   name: 'IndexPage',
   data () {
     return {
+      phrase: null,
       showMsg: false,
       messages: [
         'for a better day. for a broken sky.',
@@ -38,15 +39,29 @@ export default {
         'sweet blossom~',
         'i don\'t remember those days when my parents would kiss me goodnight every night.',
         '17',
-        'happiness.toString() returns null',
+        'happiness.toString() returns null to me.',
         'find your shelter.',
         'i don\'t know what a family is.',
-        'have you really found solace in the dark?'
+        'have you really found solace in the dark?',
+        'it\'s snowing out there.',
+        'hey, wanna play some smash?',
+        'remember what i said about that girl? i lied.',
+        'sorry about that, boy.',
+        'congratulations! you found the secret phrase!',
+        'midnight is the answer.',
+        '0xz28.'
       ]
     }
   },
   mounted () {
+    // this.storeToken()
     this.showMsg = true
+  },
+  methods: {
+    storeToken (token) {
+      const get = localStorage.getItem('phrase')
+      if (!get) { localStorage.setItem('phrase', Math.floor(Math.random() * this.messages.length)) } else { this.phrase = get }
+    }
   }
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         }
       },
       fontFamily: {
-        sans: ['IBM Plex Mono', 'Fira Code', 'sans-serif']
+        sans: ['"IBM Plex Mono"', 'Fira Code', 'sans-serif']
       }
     }
   },
