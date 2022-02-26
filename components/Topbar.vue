6610f1d15f84c5814352ada668c9a218b25020c2
ㅤㅤ <hi@aexhell.ml>
2022-02-26 02:36:25 +0100

    chore: remove home of topbar
    more space for mobile users!

--- components/Topbar.vue
+++ components/Topbar.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="phb__topbar" class="fixed border-b border-midnight-200 bg-midnight-100 flex flex-row justify-between p-12 w-full">
     <div class="phb__topbar-left">
-      <h1 class="text-midnight-600">
+      <nuxt-link to="/" class="text-midnight-600">
         aexhell.
-      </h1>
+      </nuxt-link>
     </div>
     <div class="phb__topbar-right flex flex-row">
-      <nuxt-link to="/" class="ml-6">
-        home.
-      </nuxt-link>
       <nuxt-link to="/about" class="ml-6">
         about.
       </nuxt-link>
