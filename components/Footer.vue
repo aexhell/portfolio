4c7e7b099ee5e160faeccb3906604198f782c41c
ㅤㅤ <hi@aexhell.life>
2022-07-26 12:32:39 +0200

    chore: footer border

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="axhl__footer" class="absolute bottom-15 bg-midnight-100 flex flex-row md:justify-between justify-center p-6 w-full">
+  <div id="axhl__footer" class="absolute bottom-15 border-t border-midnight-200 bg-midnight-100 flex flex-row md:justify-between justify-center p-6 w-full">
     <h1 class="text-midnight-500">
       made by <a href="https://github.com/aexhell">aexhell</a>.
     </h1>
