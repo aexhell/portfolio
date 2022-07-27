802077dc8cf0a02ae610dcf4fcd99c316bbf2bc4
ㅤㅤ <hi@aexhell.life>
2022-07-27 22:45:32 +0200

    chore: footer logo

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="axhl__footer" class="absolute bottom-15 border-t border-midnight-200 bg-midnight-100 flex flex-row md:justify-between justify-center p-6 w-full">
+  <div id="axhl__footer" class="absolute bottom-15 border-t border-midnight-200 bg-midnight-100 flex md:flex-row flex-col md:justify-between justify-center items-center py-6 px-12 w-full">
     <h1 class="text-midnight-500">
       made by <a href="https://github.com/aexhell">aexhell</a>.
     </h1>
+    <a href="https://github.com/aexhell">
+      <svg class="hidden md:block text-midnight-500 hover:text-midnight-700" xmlns="http://www.w3.org/2000/svg" width="59" height="83" viewBox="0 0 109 83" fill="none">
+        <path d="M54.5 0L81.1981 39.9096L90.9238 53.9594H81.1981L109 83V53.9594L81.1981 39.9096H65.7513L54.5 26.4176L42.1045 39.9096L27.25 40.1885L0 53.9594V83L27.25 53.9594H16.932L27.25 40.1885L54.5 0Z" fill="currentColor" />
+      </svg>
+    </a>
   </div>
 </template>
 
