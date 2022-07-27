5b8b97b0fdeea0d15da4b8a09b22ef2d63b29fa6
ㅤㅤ <hi@aexhell.life>
2022-07-27 21:53:35 +0200

    pages(index): add slide down button

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,30 @@
       <h1 id="__title-stroke" class="text-6xl font-bold text-midnight-900">
         aexhell.
       </h1>
+      <div class="flex flex-col justify-center items-center mt-4">
+        <svg
+          class="md:hidden block slidedown"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          version="1.1"
+          x="0px"
+          y="0px"
+          width="50px"
+          height="50px"
+          viewBox="0 0 960 560"
+          enable-background="new 0 0 960 560"
+          xml:space="preserve"
+          @click="goto('data-container')"
+        >
+          <g id="Rounded_Rectangle_33_copy_4_1_">
+            <path class="text-midnight-400" fill="currentColor" d="M480,344.181L268.869,131.889c-15.756-15.859-41.3-15.859-57.054,0c-15.754,15.857-15.754,41.57,0,57.431l237.632,238.937   c8.395,8.451,19.562,12.254,30.553,11.698c10.993,0.556,22.159-3.247,30.555-11.698l237.631-238.937   c15.756-15.86,15.756-41.571,0-57.431s-41.299-15.859-57.051,0L480,344.181z" />
+          </g>
+        </svg>
+      </div>
       <span class="md:hidden block select-none absolute w-min right-6 text-right text-midnight-400">/\\ \\\\ · / /// - // \ /\\ \\\\ · / /// - // \ /\\ \\\\ · / /// - // \ /\ /\\ \\\\ · / /// - // \ /\\ \\\\ · / /// - // \ /\\ \\\\</span>
     </div>
     <div class="flex p-12 flex-col w-full overflow-x-hidden">
-      <div class="__axhl-container">
+      <div ref="data-container" class="__axhl-container">
         <h1 class="md:text-4xl text-3xl font-bold text-midnight-900">
           hello.
         </h1>
@@ -43,6 +63,14 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods: {
+    goto (refName) {
+      const elem = this.$refs[refName]
+      const top = elem.offsetTop
+
+      window.scrollTo(0, top)
+    }
+  }
 }
 </script>
